1bf53c994418a378338b97680d748ed2dca0cd94
LiAlH4 <lialh4qwq@outlook.com>
2025-10-16 21:34:05 +0800

    reflactor in effect-ts

--- src/index.ts
+++ src/index.ts
@@ -1,270 +1,167 @@
-type Result<V, E> = {
-    pass: true
-    value: V
-    next: <T, A>(f: (value: V) => Result<T, A>) => Result<T, A>
-    nextAsync: <T, A>(f: (value: V) => Promise<Result<T, A>>) => Promise<Result<T, A>>
-    transError: <A>(f: (error: E) => A) => Result<V, never>
-} | {
-    pass: false
-    error: E
-    next: <T, A>(f: (value: V) => Result<T, A>) => Result<never, E>
-    nextAsync: <T, A>(f: (value: V) => Promise<Result<T, A>>) => Promise<Result<never, E>>
-    transError: <A>(f: (error: E) => A) => Result<never, A>
-}
-type ElementNotFoundError = "ElementNotFoundError"
+import { Array, Console, Duration, Effect, Option } from "effect"
 
-type resultPass = <V>(value: V) => Result<V, never>
-type resultError = <E>(error: E) => Result<never, E>
-type errorElementNotFound = () => ElementNotFoundError
-type findElement = (on: Element) => (selector: string) =>
-    Result<Element, ElementNotFoundError>
-type findElements = (on: Element) => (selector: string) =>
-    Result<Element[], ElementNotFoundError>
-type getNextCourse = (courses: HTMLSpanElement[]) => Result<HTMLSpanElement, ElementNotFoundError>
-
-type log = (msg: string) => Promise<void>
-type println = (msg: string) => Promise<void>
-type sleep = (sec: number) => Promise<void>
-type waitElement = (sec: number) => (on: Element) =>
-    (locator: () => Result<Element, ElementNotFoundError>) =>
-        Promise<Result<Element, ElementNotFoundError>>
-
-type docModifier = (doc: Document) => Promise<void>
-type getCourses = (doc: Document) =>
-    Promise<Result<HTMLSpanElement[], ElementNotFoundError>>
-
-const resultPass: resultPass = (value) => ({
-    pass: true,
-    value,
-    next: (f) => f(value),
-    nextAsync: async (f) => await f(value),
-    transError: (_) => resultPass(value)
-})
-
-const resultError: resultError = (error) => ({
-    pass: false,
-    error,
-    next: (_) => resultError(error),
-    nextAsync: async (_) => resultError(error),
-    transError: (f) => resultError(f(error))
-})
-
-const errorElementNotFound: errorElementNotFound = () => "ElementNotFoundError"
-
-const findElement: findElement = (on) => (selector) => {
-    const rawResult = on.querySelector(selector)
-    if (rawResult === null) return resultError(errorElementNotFound())
-    return resultPass(rawResult)
+interface Cons<T> {
+    new (...args: unknown[]): T
 }
 
-const findElements: findElements = (on) => (selector) => {
-    const rawResult = on.querySelectorAll(selector)
-    const rawResultArray = Array.from(rawResult)
-    if (rawResultArray.length <= 0) return resultError(errorElementNotFound())
-    return resultPass(rawResultArray)
-}
+const main = (doc: Document) =>
+    Effect.gen(function* () {
+        yield* fireMouseout(doc)
+        yield* videoAutoFinish(doc)
+    })
 
-const getNextCourse: getNextCourse = (courses) => {
-    const curCourse = courses.filter(course => course.parentElement?.classList.contains("posCatalog_active")).at(0)!
-    log(curCourse.textContent)
-    const curIndex = courses.indexOf(curCourse)
-    log(curIndex as unknown as string)
-    if (curIndex >= courses.length) return resultError(errorElementNotFound())
-    log(courses.at(curIndex + 1)!.textContent)
-    return resultPass(courses.at(curIndex + 1)!)
-}
+const fireMouseout = (doc: Document) =>
+    Effect.sync(() =>
+        doc.addEventListener(
+            "mouseout",
+            event => event.stopPropagation(),
+            true,
+        ),
+    ).pipe(Effect.flatMap(_ => log("mouseout event fired!")))
 
-const log: log = async (msg) => println(`[No Mouseout] ${msg}`)
-
-const println: println = async (msg) => console.log(msg)
-
-const sleep: sleep = (sec) =>
-    new Promise(resolve => setTimeout(() => resolve(), sec * 1000))
-
-const waitElement: waitElement = (sec) => (on) => (locator) => {
-    const tryFirst = locator()
-    if (tryFirst.pass) return Promise.resolve(tryFirst)
-    const timer = sleep(sec)
-        .then(_ => resultError(errorElementNotFound())) as
-        Promise<Result<never, ElementNotFoundError>>
-    const waiter = new Promise(resolve => {
-        const observer = new MutationObserver(_ => {
-            const trying = locator()
-            if (trying.pass) {
-                observer.disconnect()
-                resolve(trying as Result<Element, never>)
-            }
+const videoAutoFinish = (doc: Document) =>
+    Effect.gen(function* () {
+        const body = doc.body
+        const waitEle10s = waitEle(10)
+        const waitIf10sOnBody = waitEle10s(body)(HTMLIFrameElement)
+        const iframeLevel1 = yield* waitIf10sOnBody("#iframe")
+        yield* log("found iframe level 1!")
+        const ifLv1W = yield* getConW(iframeLevel1)
+        const waitIf10sOnIfLv1 = waitEle10s(ifLv1W.document.body)(
+            ifLv1W.HTMLIFrameElement,
+        )
+        const iframeLevel2 = yield* waitIf10sOnIfLv1("iframe")
+        yield* log("found iframe level 2")
+        const ifLv2W = yield* getConW(iframeLevel2)
+        const waitVd10sOnIfLv2 = waitEle10s(ifLv2W.document.body)(
+            ifLv2W.HTMLVideoElement,
+        )
+        const video = yield* waitVd10sOnIfLv2("#video_html5_api")
+        yield* log("found video")
+        yield* Effect.sleep(Duration.seconds(10))
+        const courses = yield* getCourses(doc)
+        yield* addRefreshing(courses)
+        const nextCourse = yield* getNextCourse(courses)
+        yield* playVideo(video)
+        yield* minVideoVol(video)
+        yield* addEL(video)("ended")(_ => {
+            const bjIframe = doc.createElement("iframe")
+            bjIframe.src =
+                "//music.163.com/outchain/player?type=2&id=2541479&auto=1"
+            body.appendChild(bjIframe)
+            log("injected a banjiang iframe!").pipe(Effect.runFork)
+            setTimeout(() => nextCourse.click(), 10 * 1000)
         })
-        observer.observe(on, {
-            childList: true,
-            subtree: true
-        })
-    }) as Promise<Result<Element, never>>
-    return Promise.race([timer, waiter])
+        yield* log("video injected!")
+    })
+
+const addRefreshing = (courses: HTMLSpanElement[]) =>
+    Effect.sync(() =>
+        courses.map(course =>
+            course.addEventListener("click", _ =>
+                setTimeout(() => location.reload(), 10 * 1000),
+            ),
+        ),
+    ).pipe(Effect.asVoid)
+
+const getNextCourse = (courses: HTMLSpanElement[]) =>
+    Array.findFirstIndex(
+        courses,
+        course =>
+            course.parentElement?.classList.contains("posCatalog_active") ??
+            false,
+    ).pipe(
+        Option.match({
+            onNone: () => Effect.fail(eleNotFoundError()),
+            onSome: curIndex => {
+                if (curIndex >= courses.length - 1)
+                    return Effect.fail(eleNotFoundError())
+                return Effect.succeed(courses.at(curIndex + 1)!)
+            },
+        }),
+    )
+
+const getCourses = (doc: Document) =>
+    waitEle(10)(doc.body)(HTMLDivElement)("#coursetree").pipe(
+        Effect.flatMap(ls => findEles(ls)(HTMLSpanElement)(".posCatalog_name")),
+    )
+
+const log = (msg: string) =>
+    Effect.succeed(msg).pipe(
+        Effect.map(msg => `[No Mouseout] ${msg}`),
+        Effect.flatMap(Console.log),
+    )
+
+const getConW = (iframe: HTMLIFrameElement) => {
+    const t = iframe.contentWindow
+    if (t === null) return Effect.fail(eleNotFoundError())
+    return Effect.succeed(t as Window & typeof globalThis)
 }
 
-const main: docModifier = async (doc) => {
-    fireMouseout(doc)
-    //addRefreshing(doc)
-    notifyVideoFinish(doc)
-}
+const minVideoVol = (video: HTMLVideoElement) =>
+    Effect.sync(() => {
+        video.volume = 0.01
+    })
 
-const fireMouseout: docModifier = async (doc) => {
-    doc.addEventListener("mouseout", event => {
-        event.stopPropagation();
-    }, true)
-    log("mouseout event fired!")
-}
+const playVideo = (video: HTMLVideoElement) =>
+    Effect.promise(() => video.play())
 
-const notifyVideoFinish: docModifier = async (doc) => {
-    const body = doc.body
-    const findOnBody = findElement(body)
-    const waitEle10sOnBody = waitElement(10)(body)
-    const maybeIframeLevel1 = await waitEle10sOnBody(() => findOnBody("#iframe"))
-    maybeIframeLevel1
-        .next(iframeLevel1Elem => {
-            const iframeLevel1 = iframeLevel1Elem as HTMLIFrameElement
-            const docInnerLevel1 = iframeLevel1.contentDocument
-            if (docInnerLevel1 === null) {
-                iframeLevel1.addEventListener("load", (_) => handleDocInnerLevel1(docInnerLevel1 as unknown as Document))
-                log("Doc Inner Level 1 invalid, it may be still loading, tried adding an event listener")
-            } else {
-                handleDocInnerLevel1(docInnerLevel1)
-            }
-            return resultPass(iframeLevel1Elem)
-        })
-        .transError(error => {
-            log("Iframe Level 1 not found!")
-            return error
-        })
-}
+const addEL =
+    (ele: Element) => (eName: string) => (eL: (e: Event) => unknown) =>
+        Effect.sync(() => ele.addEventListener(eName, eL))
 
-const handleDocInnerLevel1: docModifier = async (doc) => {
-    log("Doc Inner Level 1 injected!")
-    const body = doc.body
-    const findOnBody = findElement(body)
-    const waitEle10sOnBody = waitElement(10)(body)
-    const maybeIframeLevel2 = await waitEle10sOnBody(() => findOnBody("iframe"))
-    maybeIframeLevel2
-        .next(iframeLevel2Elem => {
-            const iframeLevel2 = iframeLevel2Elem as HTMLIFrameElement
-            const docInnerLevel2 = iframeLevel2.contentDocument
-            if (docInnerLevel2 === null) {
-                iframeLevel2.addEventListener("load", (_) => handleDocInnerLevel2(docInnerLevel2 as unknown as Document))
-                log("Doc Inner Level 2 invalid, it may be still loading, tried adding an event listener")
-            } else {
-                handleDocInnerLevel2(docInnerLevel2)
-            }
-            return resultPass(iframeLevel2Elem)
-        })
-        .transError(error => {
-            log("Iframe Level 2 not found!")
-            return error
-        })
-}
-
-const handleDocInnerLevel2: docModifier = async (doc) => {
-    log("Doc Inner Level 2 injected!")
-    const body = doc.body
-    const findOnBody = findElement(body)
-    const waitEle10sOnBody = waitElement(10)(body)
-    const maybeVideo = await waitEle10sOnBody(() => findOnBody("#video_html5_api"))
-    await sleep(10)
-    maybeVideo
-        .next(videoElem => {
-            const video = videoElem as HTMLVideoElement
-            video.play()
-            video.volume = 0.01
-            video.addEventListener("ended", async _ => {
-                const banjiangIframe = document.createElement("iframe")
-                banjiangIframe.src = "//music.163.com/outchain/player?type=2&id=2541479&auto=1"
-                body.appendChild(banjiangIframe)
-                log("tried injecting a banjiang music player!")
-                await sleep(10)
-                nextCourse(document)
-            })
-            return resultPass(videoElem)
-        })
-        .transError(error => {
-            log("Video not found!")
-            return error
-        })
-}
-
-const addRefreshing: docModifier = async (doc) => {
-    const body = doc.body
-    const findOnBody = findElement(body)
-    const waitEle10s = waitElement(10)
-    const waitEle10sOnBody = waitEle10s(body)
-    const maybeCourseList = await waitEle10sOnBody(() => findOnBody("#coursetree"))
-    await sleep(10)
-    maybeCourseList
-        .next(courseListElem => {
-            const findsOnCourseList = findElements(courseListElem)
-            const maybeCourses = findsOnCourseList(".posCatalog_name")
-            maybeCourses
-                .next(courses => {
-                    log(courses.length as unknown as string)
-                    courses.map(course => {
-                        course.addEventListener("click", (_) => doc.location.reload())
-                        log(course as unknown as string)
+const waitEle =
+    (sec: number) =>
+    (on: Element) =>
+    <E extends Element>(what: Cons<E>) =>
+    (sel: string) =>
+        findEle(on)(what)(sel).pipe(
+            Effect.orElse(() =>
+                Effect.async<E, string>(resume => {
+                    const f = () => {
+                        const t = on.querySelector(sel)
+                        if (t !== null && eleIs(what)(t))
+                            resume(Effect.succeed(t))
+                    }
+                    const obs = new MutationObserver(_ => f())
+                    obs.observe(on, {
+                        childList: true,
+                        subtree: true,
                     })
-                    log("added refreshing for every course!")
-                    return resultPass(courses)
-                })
-                .transError(error => {
-                    log("Course List is empty!")
-                    return error
-                })
-            return resultPass(courseListElem)
-        })
-        .transError(error => {
-            log("Course List not found!")
-            return error
-        })
-}
+                    f()
+                    return Effect.sync(() => obs.disconnect())
+                }).pipe(
+                    Effect.timeoutFail({
+                        duration: Duration.seconds(sec),
+                        onTimeout: () => eleNotFoundError(),
+                    }),
+                ),
+            ),
+        )
 
-const nextCourse: docModifier = async (doc) => {
-    const maybeCourses = await getCourses(doc)
-    maybeCourses
-        .next(courses => {
-            const maybeNextCourse = getNextCourse(courses)
-            maybeNextCourse
-                .next(nextCourse => {
-                    log(nextCourse as unknown as string)
-                    nextCourse.click()
-                    sleep(10).then(_ => doc.location.reload())
-                    return resultPass(nextCourse)
-                })
-                .transError(error => {
-                    log("no next course, finale!")
-                    return error
-                })
-            return resultPass(courses)
-        })
-        .transError(error => {
-            log("can't get courses!")
-            return error
-        })
-}
+const findEles =
+    (on: Element) =>
+    <E extends Element>(what: Cons<E>) =>
+    (sel: string) => {
+        const ts = Array.fromIterable(on.querySelectorAll(sel))
+        if (ts.length >= 1 && ts.every(eleIs(what))) return Effect.succeed(ts)
+        return Effect.fail(eleNotFoundError())
+    }
 
-const getCourses: getCourses = async (doc) => {
-    const body = doc.body
-    const findOnBody = findElement(body)
-    const waitEle10s = waitElement(10)
-    const waitEle10sOnBody = waitEle10s(body)
-    const maybeCourseList = await waitEle10sOnBody(() => findOnBody("#coursetree"))
-    await sleep(10)
-    return maybeCourseList
-        .next(courseListElem => {
-            const findsOnCourseList = findElements(courseListElem)
-            const maybeCourses = findsOnCourseList(".posCatalog_name")
-            return maybeCourses as Result<HTMLSpanElement[], ElementNotFoundError>
-        })
-        .transError(error => {
-            log("Course List not found!")
-            return error
-        })
-}
+const findEle =
+    (on: Element) =>
+    <E extends Element>(what: Cons<E>) =>
+    (sel: string) => {
+        const t = on.querySelector(sel)
+        if (t !== null && eleIs(what)(t)) return Effect.succeed(t)
+        return Effect.fail(eleNotFoundError())
+    }
 
-main(document)
+const eleIs =
+    <E extends Element>(cons: Cons<E>) =>
+    (ele: Element): ele is E =>
+        ele instanceof cons
+
+const eleNotFoundError = () => "ElementNotFound"
+
+main(document).pipe(Effect.runFork)
